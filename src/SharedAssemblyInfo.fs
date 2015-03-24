680c3a3686a47579c5ff76b0e3831d8c204144b1
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-25 00:28:09 +0100

    Bump version to 1.0.2-beta1

--- src/SharedAssemblyInfo.fs
+++ src/SharedAssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Yaaf.Sasl.Ldap")>]
 [<assembly: AssemblyProductAttribute("Yaaf.Sasl.Ldap")>]
 [<assembly: AssemblyCopyrightAttribute("Yaaf.Sasl.Ldap Copyright © Matthias Dittrich 2011-2015")>]
-[<assembly: AssemblyVersionAttribute("1.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.0.1")>]
+[<assembly: AssemblyVersionAttribute("1.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.0.2-beta1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.1"
+    let [<Literal>] Version = "1.0.2"
